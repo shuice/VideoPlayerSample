154a7c536a6c2065478230da92e77562b59ba06e
shuice <shuice@391e3dc2-b421-4241-b4d1-5d4d07a1dce3>
2012-02-08 16:25:25 +0000

    忘记提交

--- UnitTest/ViewController.mm
+++ UnitTest/ViewController.mm
@@ -76,7 +76,6 @@
 //    smiSubTitleReader.LoadFile("/Users/xiaoyi/Downloads/1s/101.big5.smi");
     PlayerViewController* viewController = [PlayerViewController sharedPlayer];
     [[self navigationController] pushViewController:viewController animated:YES];
-    [viewController open:@"/Users/xiaoyi/Test.flv"];
 }
 
 @end
